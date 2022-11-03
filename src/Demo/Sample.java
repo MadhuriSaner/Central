315a4326134f62ea24b6fe854401903657ea9ec8
shind <shind@LAPTOP-3B04Q1EJ>
2022-11-03 06:14:24 +0530

    swapping

---
+++ src/Demo/Sample.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class Sample 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+	    a=a+b;   //30
+	    b=a-b;   //10
+	    a=a-b;   //20
+	    
+	    System.out.println("a="+a);
+	    System.out.println("b="+b);
+	    
+		
+		
+	}
+
+}
